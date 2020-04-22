1c8517e7adac7fe44397b9e941772492337f727e
lucasgabrielsantos <lucas.gsantos@live.com>
2020-04-22 15:51:57 -0300

    criando alguns testes com assinaturas bem intuitivas para melhor  entendimento

--- Leilao/app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ Leilao/app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -6,8 +6,9 @@ import static org.junit.Assert.assertEquals;
 
 public class LeilaoTest {
 
+    // [nomedometodo][Estado do teste][resultado esperado]
     @Test
-    public void getDescricao() {
+    public void getDescricaoRecebeDescricaoDevolveDescricao() {
         // criar cenário de teste
         Leilao console = new Leilao("Console");
 
@@ -19,14 +20,38 @@ public class LeilaoTest {
     }
 
     @Test
-    public void getMaiorLance() {
+    public void getMaiorLanceQuandoRecebeApenasUmLanceDevolveMaiorLance() {
+
         Leilao console = new Leilao("Console");
         console.propoe(new Lance(new Usuario("Lucas"), 200.0));
 
-        double maiorLanceDevolvido = console.getMaiorLance();
+        double maiorLanceDevolvidoDoConsole = console.getMaiorLance();
 
-        assertEquals(200.0, maiorLanceDevolvido, 0.0001);
+        assertEquals(200.0, maiorLanceDevolvidoDoConsole, 0.0001);
 
+    }
 
+    @Test
+    public void getMaiorLanceQuandoRecebeMaisDeUmLanceEmOrdemCrescenteDevolveMaiorLance(){
+
+        Leilao computador = new Leilao("Computador");
+        computador.propoe(new Lance(new Usuario("Lucas"), 100.0));
+        computador.propoe(new Lance(new Usuario("Gabriel"), 200.0));
+
+        double maiorLanceDevolvidoDoComputador = computador.getMaiorLance();
+
+        assertEquals(200.0, maiorLanceDevolvidoDoComputador, 0.0001);
+    }
+
+    @Test
+    public void getMaiorLanceQuandoRecebeMaisDeUmLanceEmOrdemDecrescenteDevolveMaiorLance(){
+
+        Leilao carro = new Leilao("Carro");
+        carro.propoe(new Lance(new Usuario("Santos"), 10000.0));
+        carro.propoe(new Lance(new Usuario("Anderson"), 9000.0));
+
+        double maiorLanceDevolvidoDoCarro = carro.getMaiorLance();
+
+        assertEquals(10000.0, maiorLanceDevolvidoDoCarro, 0.0001);
     }
 }
