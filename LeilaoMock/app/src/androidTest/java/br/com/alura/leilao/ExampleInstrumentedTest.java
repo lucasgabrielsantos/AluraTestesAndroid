86279d7f2a2e0454ef80a58cfbf6b393a067caf4
lucasgabrielsantos <lucas.gsantos@live.com>
2020-05-11 15:05:36 -0300

    realizando o primeiro teste no uso do mockito em testes unitarios no adapter

--- LeilaoMock/app/src/androidTest/java/br/com/alura/leilao/ExampleInstrumentedTest.java
+++ LeilaoMock/app/src/androidTest/java/br/com/alura/leilao/ExampleInstrumentedTest.java
@@ -7,15 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import br.com.alura.leilao.model.Leilao;
-import br.com.alura.leilao.ui.recyclerview.adapter.ListaLeilaoAdapter;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -29,14 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(appContext);
-
-        adapter.atualiza(new ArrayList<>(Arrays.asList(
-                new Leilao("Console"),
-                new Leilao("Computador")
-        )));
-        int quantidadeLeiloesDevolvida = adapter.getItemCount();
-
-        assertThat(quantidadeLeiloesDevolvida, is(equalTo(2)));
+        assertEquals("br.com.alura.leilao", appContext.getPackageName());
     }
 }
