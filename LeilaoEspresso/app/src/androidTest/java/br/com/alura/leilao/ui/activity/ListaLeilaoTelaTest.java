faef5c5e356dfbe65dce23658c73cb1e8c1bed13
lucasgabrielsantos <lucas.gsantos@live.com>
2020-05-22 10:53:37 -0300

    criando o primeiro teste instrumentado

---
+++ LeilaoEspresso/app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -0,0 +1,28 @@
+package br.com.alura.leilao.ui.activity;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ListaLeilaoTelaTest {
+
+    @Rule
+    public ActivityTestRule activity =
+            new ActivityTestRule(ListaLeilaoActivity.class, true, true);
+
+    @Test
+
+    public void deve_AparecerUmLeilao_QuandoCarregarUmLeilaoNaApi() {
+        Espresso.onView(ViewMatchers.withText("Computador"))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        Espresso.onView(ViewMatchers.withText("Console"))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        Espresso.onView(ViewMatchers.withText("Carro"))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+}
